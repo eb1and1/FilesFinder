101133900692452ec4b79865bdecda053c477421
eb1and1 <evan.brenguier@jtekt.eu>
2025-10-29 17:04:22 +0100

    Add files via upload

---
+++ filesfinder.ps1
@@ -0,0 +1,15 @@
+﻿$DossierSource = "C:\Users\$env:username\Desktop\CyberMonth\Scripts"
+
+$FichierSortie = "C:\Users\$env:username\ListeFichiers.txt"
+
+# Récupérer tous les fichiers du dossier (et sous-dossiers si désiré)
+# Enlever -Recurse si vous ne voulez que les fichiers du dossier principal
+$ListeFichiers = Get-ChildItem -Path $DossierSource -File -Recurse
+
+# Extraire uniquement les noms complets des fichiers
+$ListeNoms = $ListeFichiers.FullName
+
+# Écrire la liste dans le fichier texte
+$ListeNoms | Out-File -FilePath $FichierSortie -Encoding UTF8
+
+notepad "C:\Users\$env:username\ListeFichiers.txt"
