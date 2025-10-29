8781c2b8bb568dda18fa2809d7fba8b68d963853
eb1and1 <evan.brenguier@jtekt.eu>
2025-10-29 17:06:01 +0100

    Add files via upload

---
+++ filesfinder.ps1
@@ -0,0 +1,11 @@
+﻿$DossierSource = "C:\Users\$env:username\Desktop\CyberMonth\Scripts"
+
+$FichierSortie = "C:\Users\$env:username\ListeFichiers.txt"
+
+$ListeFichiers = Get-ChildItem -Path $DossierSource -File -Recurse
+
+$ListeNoms = $ListeFichiers.FullName
+
+$ListeNoms | Out-File -FilePath $FichierSortie -Encoding UTF8
+
+notepad "C:\Users\$env:username\ListeFichiers.txt"
